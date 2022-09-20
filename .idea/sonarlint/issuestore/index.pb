663edbc641aa4632b08037e5c99f08536aa8334d
mazeu <marciomazeu@hotmail.com>
2022-09-20 08:11:53 -0300

    DepartmentDao implementation

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+=
+db.properties,3\b\3b8c33088fa60dfa967fdedbf4c75fa52fdece9a
+P
+ src/model/entity/Department.java,a\0\a017bfddabc3355eb8771f38ff44c8c0261b4343
+L
+src/model/entity/Seller.java,b\4\b49f0e62dc81438b936cfa86409c739e0bc61d67
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 import java.sql.SQLException;
@@ -9,5 +10,9 @@ public class DaoFactory {
 
     public static SellerDao createSellerDao() throws SQLException {
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDao createDepartmentDao() throws SQLException {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
